d1fb91f52ca957b1b99449a777fe8fff4856a774
W. Conor McFerren <cnmcferren@lightning.local>
2021-03-25 22:54:44 -0400

    more complicated

--- example.c
+++ example.c
@@ -1,4 +1,11 @@
 int main(void) {
     int x = 7;
+    int running = 1;
+    while (running) {
+        x = x + 1;
+        if (x == 10) {
+            running = 0;
+        }
+    }
     return x;
 }
