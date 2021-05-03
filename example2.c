99b7cf5bd312db4d993afd8bec17af9ce36f5d0c
cnmcferren <cnmcferren@gmail.com>
2021-05-03 15:09:51 -0700

    no

--- example2.c
+++ example2.c
@@ -1,16 +1,5 @@
-int test(void) {
-    int a = 0;
-    return a;
-}
-
-int anotherTest(int z) {
-    z = 5;
-    return z;
-}
-
 int main(void) {
     int x = 5;
     int y = 6;
-    test();
     return 0;
 }
