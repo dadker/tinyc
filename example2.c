3f071daaad660467552904f4785193753e827b14
cnmcferren <cnmcferren@gmail.com>
2021-05-03 15:11:58 -0700

    yeah

--- example2.c
+++ example2.c
@@ -1,14 +1,7 @@
 int main(void) {
-<<<<<<< HEAD
-    printf("Hello.");
-=======
     int x = 5;
     int y = 6;
-<<<<<<< HEAD
-=======
     printf("Hello");
     printf("World");
->>>>>>> 5ed85913b500d0f3a94db15c059cfc8bb2b63e13
->>>>>>> cf49ac0ed601d0bd5ab1d89ea4de63f0f6dad827
     return 0;
 }
